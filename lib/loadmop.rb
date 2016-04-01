76fe43be91081f5b3648162b5c29975689a54be6
Ryan Duryea <aguynamedryan@gmail.com>
2016-04-01 15:35:35 -0700

    Simplify the CLI

--- lib/loadmop.rb
+++ lib/loadmop.rb
@@ -1,5 +1,14 @@
 require 'loadmop/version'
+require 'loadmop/cdmv4_loader'
+require 'loadmop/cdmv4_plus_loader'
+require 'loadmop/vocab_loader'
 
 module Loadmop
-  # Your code goes here...
+  def self.loader(schema)
+    {
+      vocab: VocabLoader,
+      cdmv4: CDMv4Loader,
+      cdmv4_plus: CDMv4PlusLoader
+    }[schema.to_sym]
+  end
 end
--- lib/loadmop/cli.rb
+++ lib/loadmop/cli.rb
@@ -1,11 +1,8 @@
 require 'thor'
-require_relative 'cdmv4_loader'
-require_relative 'vocab_loader'
+require 'loadmop'
 
 module Loadmop
   class CLI < Thor
-    include Sequelizer
-
     class_option :adapter,
       aliases: :a,
       desc: 'adapter for database'
@@ -28,15 +25,9 @@ module Loadmop
       aliases: :s,
       desc: 'schema for database (PostgreSQL and SQL Server only)'
 
-    desc 'create_vocab_database database_name vocab_files_dir', 'Creates the vocabulary tables in database_name and loads the OMOP Vocabulary data into them.'
-    def create_vocab_database(database_name, vocab_files_dir)
-      loader = VocabLoader.new(database_name, vocab_files_dir, options)
-      loader.create_database
-    end
-
-    desc 'create_cdmv4_database database_name data_dir', 'Creates a set of tables in database_name for OMOP CDMv4 then loads data into them as specified by data_files_dir.'
-    def create_cdmv4_database(database_name, data_files_dir)
-      loader = CDMv4Loader.new(database_name, data_files_dir, options)
+    desc 'create {cdmv4 cdmv4_plus, vocab} database_name files_dir', 'Creates the tables specified in database_name and loads the files specified into them'
+    def create(schema, database_name, files_dir)
+      loader = Loadmop.loader(schema).new(database_name, files_dir, options)
       loader.create_database
     end
   end
