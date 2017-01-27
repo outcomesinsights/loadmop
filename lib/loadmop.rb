bf7b19a82b8acd7dd6fb05c3b9cf9a5a4bdc1f97
Ryan Duryea <aguynamedryan@gmail.com>
2017-01-26 16:19:20 -0800

    Relocate a few loaders in prep for some refactoring

--- lib/loadmop.rb
+++ lib/loadmop.rb
@@ -1,8 +1,8 @@
 require 'loadmop/version'
-require 'loadmop/cdmv4_loader'
-require 'loadmop/cdmv4_plus_loader'
-require 'loadmop/vocab_loader'
-require 'loadmop/generic_loader'
+require 'loadmop/loaders/cdmv4_loader'
+require 'loadmop/loaders/cdmv4_plus_loader'
+require 'loadmop/loaders/vocab_loader'
+require 'loadmop/loaders/generic_loader'
 
 module Loadmop
   def self.loader(schema)
